5804334e24b368fce4b56fa53193453db4ce8401
ShutovKS <schutow.kir@yandex.ru>
2024-10-21 23:46:09 +0300

    Add nodemon in price-watcher-service

--- src/price-watcher-service/index.js
+++
@@ -1,7 +0,0 @@
-import parse_price_joom from "./webpages/handler_joom.js";
-
-parse_price_joom('https://www.joom.com/ru-ua/products/64d738fbd95dd501e48b5298').then(result => {
-    console.log(result);
-}).catch(err => {
-    console.error(err);
-});
